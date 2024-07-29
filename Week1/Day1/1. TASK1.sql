aef2ba2b6f3fc4dbd542b25ea1578d3cf04cc8a9
K. Dushyant Reddy <dushireddy291@gmail.com>
2024-07-29 16:15:13 +0530

    Readme added

--- Week1/Day1/1. TASK1.sql
+++ Week1/Day1/1. TASK1.sql
@@ -1,5 +1,13 @@
 /* Author - K. Dushyant Reddy */
 
+/* Problem Statement*/
+--  TASK 1;
+-- Create a database with a table named students. 
+-- The table has fields s_id (student ID), s_name (student name), and s_age (student age). 
+-- Create the table and insert the following values: (1, "Ram", 21) and (2, "Shamam", 21). 
+-- Write a SQL query to display all records present in the students table.
+
+
 /* It will create the Database named Revature */
 CREATE DATABASE Revature;
 
